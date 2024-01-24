3c8f06ae8fafc7db9e646ba2d3e4668a40775413
david esthyf renjifo flores <tj.david.renjifo.f@upds.net.bo>
2024-01-23 21:57:07 -0400

    envio tarea8

---
+++ ClientPoliclinicoLaFamilia/Models/Cita.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClientPoliclinicoLaFamilia.Models
+{
+    public class Cita
+    {
+        [Key]
+        [Column("IDCita")]
+        public int Idcita { get; set; }
+
+        [Column("IDPaciente")]
+        public int? Idpaciente { get; set; }
+
+        [Column("IDMédico")]
+        public int? Idmédico { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime? FechaHoraCita { get; set; }
+
+        [StringLength(255)]
+        [Unicode(false)]
+        public string? Motivo { get; set; }
+
+        [StringLength(20)]
+        [Unicode(false)]
+        public string? EstadoCita { get; set; }
+    }
+}
---
+++ ClientPoliclinicoLaFamilia/Services/CitaService.cs
@@ -0,0 +1,25 @@
+﻿using ClientPoliclinicoLaFamilia.Models;
+using Newtonsoft.Json;
+
+namespace ClientPoliclinicoLaFamilia.Services
+{
+    public class CitaService
+    {
+        string url = "https://localhost:7033";
+        string endPoint = "";
+        HttpClient client = new HttpClient();
+        public async Task<List<Cita>> ListaIngredientes()
+        {
+            endPoint = "api/Medico";
+            client.BaseAddress = new Uri(url);
+            HttpResponseMessage response = await client.GetAsync(endPoint);
+            List<Cita> result = new List<Cita>();
+            if (response.IsSuccessStatusCode)
+            {
+                string respuestaCuerpo = await response.Content.ReadAsStringAsync();
+                result = JsonConvert.DeserializeObject<List<Cita>>(respuestaCuerpo);
+            }
+            return result;
+        }
+    }
+}
